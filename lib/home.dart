7082126ddc4f9744aaee5e5dfd668aeaf7314534
seungmin9836 <71756020+daviddev90@users.noreply.github.com>
2022-05-17 15:29:21 +0900

    이순신

--- lib/home.dart
+++ lib/home.dart
@@ -8,41 +8,65 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.orange,
       appBar: AppBar(
+        backgroundColor: Colors.deepOrange,
         title: const Text('Flutter'),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const CircleAvatar(
-                radius: 80,
-                backgroundColor: Colors.white,
-                backgroundImage: AssetImage(
-                  'images/pikachu-1.jpg',
-                )),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Padding(
-                  padding: EdgeInsets.fromLTRB(12, 0, 12, 0),
-                  child: CircleAvatar(
-                      radius: 80,
-                      backgroundColor: Colors.white,
-                      backgroundImage: AssetImage(
-                        'images/pikachu-2.jpg',
-                      )),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(12, 0, 12, 0),
-                  child: CircleAvatar(
-                      radius: 80,
-                      backgroundColor: Colors.white,
-                      backgroundImage: AssetImage(
-                        'images/pikachu-3.jpg',
-                      )),
-                ),
-              ],
-            )
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('images/Lee.jpg'),
+                radius: 70,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(0, 8, 0, 8),
+              child: Divider(
+                thickness: 3,
+              ),
+            ),
+            const Text('성웅',
+                style: TextStyle(fontSize: 16, color: Colors.white)),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(0, 12, 0, 16),
+              child: Text('이순신 장군',
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold)),
+            ),
+            const Text('전적',
+                style: TextStyle(fontSize: 16, color: Colors.white)),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(0, 12, 0, 16),
+              child: Text('62전 62승',
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold)),
+            ),
+            ...['옥포해전', '사천포해전', '당포해전', '한산도대첩', '부산포해전', '명량해전', '노량해전']
+                .map((e) => Row(children: [
+                      const Icon(Icons.check_circle_outline, size: 20.0),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        e,
+                        style: const TextStyle(fontSize: 18),
+                      )
+                    ]))
+                .toList(),
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('images/turtle.gif'),
+                radius: 50,
+                backgroundColor: Color.fromRGBO(0, 0, 0, 0),
+              ),
+            ),
           ],
         ),
       ),
