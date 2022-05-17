f893928c5f909e753b56d08db3d20a4adfbc22d0
seungmin9836 <71756020+daviddev90@users.noreply.github.com>
2022-05-17 11:47:06 +0900

    circle

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,10 +6,46 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.orange,
       appBar: AppBar(
         title: const Text('Flutter'),
       ),
-      body: Center(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+                radius: 80,
+                backgroundColor: Colors.white,
+                backgroundImage: AssetImage(
+                  'images/pikachu-1.jpg',
+                )),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(12, 0, 12, 0),
+                  child: CircleAvatar(
+                      radius: 80,
+                      backgroundColor: Colors.white,
+                      backgroundImage: AssetImage(
+                        'images/pikachu-2.jpg',
+                      )),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(12, 0, 12, 0),
+                  child: CircleAvatar(
+                      radius: 80,
+                      backgroundColor: Colors.white,
+                      backgroundImage: AssetImage(
+                        'images/pikachu-3.jpg',
+                      )),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
