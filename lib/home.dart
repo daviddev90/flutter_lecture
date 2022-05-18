13dede6ac3f7b706767c37f18c1a9c545479a60c
seungmin9836 <71756020+daviddev90@users.noreply.github.com>
2022-05-18 11:14:12 +0900

    hahaha

--- lib/home.dart
+++ lib/home.dart
@@ -1,20 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_lecture/Pages/screen.dart';
-import 'package:flutter_lecture/Pages/screen1st.dart';
-import 'package:flutter_lecture/Pages/screen2nd.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        '/': (context) => const Screen(),
-        '/1st': (context) => const Screen1st(),
-        '/2nd': (context) => const Screen2nd(),
-      },
-      initialRoute: '/',
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('App Bar Icon'),
+        backgroundColor: Colors.amberAccent,
+        foregroundColor: Colors.black,
+        leading: IconButton(
+            onPressed: () {
+              //
+            },
+            icon: const Icon(Icons.menu)),
+        actions: [
+          IconButton(
+              onPressed: () {
+                //
+              },
+              icon: const Icon(Icons.email)),
+          IconButton(
+              onPressed: () {
+                //
+              },
+              icon: const Icon(Icons.add_alarm)),
+          IconButton(
+              onPressed: () {
+                //
+              },
+              icon: const Icon(Icons.add_outlined)),
+          GestureDetector(
+            onTap: (() {
+              print('hello');
+            }),
+            child: Image.asset(
+              'images/smile.png',
+              width: 28,
+              height: 28,
+            ),
+          ),
+          const SizedBox(
+            width: 12,
+          )
+        ],
+      ),
     );
   }
 }
