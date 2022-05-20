99fbd486788a6c7153507313f81f9fd4bd29d411
seungmin9836 <71756020+daviddev90@users.noreply.github.com>
2022-05-20 11:32:53 +0900

    sdfdfdsfsf

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_lecture/pages/bmiPage.dart';
-import 'package:flutter_lecture/pages/calPage.dart';
-import 'package:flutter_lecture/pages/homePage.dart';
+import 'package:simple_gesture_detector/simple_gesture_detector.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -11,42 +9,68 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   //property
-  int _navIdx = 0;
+  late int imgIdx;
 
-  final List<Widget> _widgets = <Widget>[
-    const HomePage(),
-    const BMIPage(),
-    const CalPage()
-  ];
+  @override
+  void initState() {
+    super.initState();
+    imgIdx = 0;
+  }
 
-  void _onNavTapped(int index) {
+  void _horswipe(SwipeDirection direction){
     setState(() {
-      _navIdx = index;
+      if(direction == SwipeDirection.left){
+        imgIdx += 1;
+        if(imgIdx >= 13){
+          imgIdx = 0;
+        }
+      }else{
+        imgIdx -= 1;
+        if(imgIdx < 0){
+          imgIdx = 12;
+        }
+      }
     });
   }
 
+  void _verswipe(SwipeDirection direction){
+    setState(() {
+      if(direction == SwipeDirection.down){
+        imgIdx += 1;
+        if(imgIdx >= 13){
+          imgIdx = 0;
+        }
+      }else{
+        imgIdx -= 1;
+        if(imgIdx < 0){
+          imgIdx = 12;
+        }
+      }
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      //scaffold를 감싸고 아래 내용을 줘야 키보드를 없앨 수 있다.
-      onTap: () {
-        FocusScope.of(context).unfocus();
-      },
-      child: Scaffold(
-        body: _widgets.elementAt(_navIdx),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.calculate), label: 'BMI계산기'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.food_bank), label: '칼로리계산기'),
-          ],
-          onTap: _onNavTapped,
-          currentIndex: _navIdx,
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Image Swiping'),
         ),
-      ),
-    );
+        body: SimpleGestureDetector(
+          onHorizontalSwipe: _horswipe,
+          onVerticalSwipe: _verswipe,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'images/$imgIdx.jpeg'
+                )
+              ],
+            ),
+          ),
+        ),
+      );
   }
 
   // -- Functions
--- lib/router.dart
+++ lib/router.dart
@@ -1,13 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_lecture/books/01.dart';
-import 'package:flutter_lecture/books/02.dart';
-import 'package:flutter_lecture/books/03.dart';
-import 'package:flutter_lecture/books/04.dart';
-import 'package:flutter_lecture/books/05.dart';
-import 'package:flutter_lecture/books/06.dart';
-import 'package:flutter_lecture/books/07.dart';
-import 'package:flutter_lecture/books/08.dart';
-import 'package:flutter_lecture/books/09.dart';
 import 'package:flutter_lecture/home.dart';
 
 class MyRouter extends StatelessWidget {
@@ -18,15 +9,6 @@ class MyRouter extends StatelessWidget {
     return MaterialApp(
       routes: {
         '/': (context) => const Home(),
-        '/01': (context) => const PushUpPage(),
-        '/02': (context) => const Page02(),
-        '/03': (context) => const Page03(),
-        '/04': (context) => const Page04(),
-        '/05': (context) => const Page05(),
-        '/06': (context) => const Page06(),
-        '/07': (context) => const Page07(),
-        '/08': (context) => const Page08(),
-        '/09': (context) => const Page09(),
       },
       initialRoute: '/',
       debugShowCheckedModeBanner: false,
