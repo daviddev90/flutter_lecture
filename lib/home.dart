8d51916e8554d5b6d83e4188d632ac6f492deffb
seungmin9836 <71756020+daviddev90@users.noreply.github.com>
2022-05-25 18:21:58 +0900

    webview

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_lecture/animal_item.dart';
-import 'package:flutter_lecture/pages/first.dart';
-import 'package:flutter_lecture/pages/second.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -10,69 +10,111 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
-  //property
-  late TabController controller;
-  List<Animal> animalList = [];
-
-  @override
-  void initState() {
-    controller = TabController(length: 2, vsync: this);
-
-    animalList.add(Animal(
-        imgPath: 'images/bee.png', name: '벌', species: '곤충', canFly: true));
-    animalList.add(Animal(
-        imgPath: 'images/cat.png', name: '고양이', species: '포유류', canFly: false));
-    animalList.add(Animal(
-        imgPath: 'images/cow.png', name: '소', species: '포유류', canFly: false));
-    animalList.add(Animal(
-        imgPath: 'images/dog.png', name: '강아지', species: '포유류', canFly: false));
-    animalList.add(Animal(
-        imgPath: 'images/fox.png', name: '여우', species: '포유류', canFly: false));
-    animalList.add(Animal(
-        imgPath: 'images/monkey.png',
-        name: '원숭이',
-        species: '영장류',
-        canFly: false));
-    animalList.add(Animal(
-        imgPath: 'images/pig.png', name: '돼지', species: '포유류', canFly: false));
-    animalList.add(Animal(
-        imgPath: 'images/wolf.png', name: '늑대', species: '포유류', canFly: false));
-
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
+class _HomeState extends State<Home> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+  bool isLoading = true;
+  String siteName = 'www.daum.net';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Listview Test'),
-      ),
-      body: TabBarView(controller: controller, children: [
-        FirstPage(list: animalList),
-        SecondPage(list: animalList)
-      ]),
-      bottomNavigationBar: TabBar(controller: controller, tabs: const [
-        Tab(
-          icon: Icon(
-            Icons.looks_one,
-            color: Colors.blue,
+        backgroundColor: Colors.teal,
+        title: Center(
+          child: Column(
+            children: [
+              const Text('WebView'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.red)),
+                      onPressed: () {
+                        setState(() {
+                          siteName = 'google.com';
+                        });
+                        _reloadSite();
+                      },
+                      child: const Text('Google')),
+                  ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.green)),
+                      onPressed: () {
+                        setState(() {
+                          siteName = 'naver.com';
+                        });
+                        _reloadSite();
+                      },
+                      child: const Text('Naver')),
+                  ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.blue)),
+                      onPressed: () {
+                        setState(() {
+                          siteName = 'daum.net';
+                        });
+                        _reloadSite();
+                      },
+                      child: const Text('Daum'))
+                ],
+              ),
+            ],
           ),
-          text: 'List ',
         ),
-        Tab(
-          icon: Icon(
-            Icons.looks_two,
-            color: Colors.red,
+        toolbarHeight: 100,
+      ),
+      body: Stack(
+        children: [
+          WebView(
+            initialUrl: 'https://$siteName',
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (WebViewController webViewController) {
+              _controller.complete(webViewController);
+            },
+            onPageFinished: (finish) {
+              setState(() {
+                isLoading = false;
+              });
+            },
+            onPageStarted: (start) {
+              setState(() {
+                isLoading = true;
+              });
+            },
           ),
-        )
-      ]),
+          isLoading
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Stack()
+        ],
+      ),
+      floatingActionButton: FutureBuilder<WebViewController>(
+          future: _controller.future,
+          builder: (BuildContext context,
+              AsyncSnapshot<WebViewController> controller) {
+            if (controller.hasData) {
+              return FloatingActionButton(
+                backgroundColor: Colors.redAccent,
+                onPressed: () {
+                  controller.data!.goBack();
+                },
+                child: const Icon(Icons.arrow_back),
+              );
+            } else {
+              return Stack();
+            }
+          }),
     );
+  }
+
+  //Functions
+  _reloadSite() {
+    _controller.future.then((value) => value.loadUrl('https://$siteName'));
   }
 }
