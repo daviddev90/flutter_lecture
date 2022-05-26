cbadc6e48c056fdc6326eb2cd0f19c32eddc97a3
seungmin9836 <71756020+daviddev90@users.noreply.github.com>
2022-05-26 11:35:57 +0900

    34c532c4234c23

--- lib/home.dart
+++ lib/home.dart
@@ -80,17 +80,43 @@ class _HomeState extends State<Home> {
               : Stack()
         ],
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FutureBuilder<WebViewController>(
           future: _controller.future,
           builder: (BuildContext context,
               AsyncSnapshot<WebViewController> controller) {
             if (controller.hasData) {
-              return FloatingActionButton(
-                backgroundColor: Colors.redAccent,
-                onPressed: () {
-                  controller.data!.goBack();
-                },
-                child: const Icon(Icons.arrow_back),
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FloatingActionButton(
+                    backgroundColor: Colors.redAccent,
+                    onPressed: () {
+                      controller.data!.goBack();
+                    },
+                    child: const Icon(Icons.arrow_back),
+                  ),
+                  const SizedBox(
+                    width: 12,
+                  ),
+                  FloatingActionButton(
+                    backgroundColor: Colors.amberAccent,
+                    onPressed: () {
+                      controller.data!.reload();
+                    },
+                    child: const Icon(Icons.replay),
+                  ),
+                  const SizedBox(
+                    width: 12,
+                  ),
+                  FloatingActionButton(
+                    backgroundColor: Colors.blue,
+                    onPressed: () {
+                      controller.data!.goForward();
+                    },
+                    child: const Icon(Icons.arrow_forward),
+                  )
+                ],
               );
             } else {
               return Stack();
