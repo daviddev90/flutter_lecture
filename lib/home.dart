dd0f7c12d8300b59c44057a1d5e2bc2e69c34256
seungmin9836 <71756020+daviddev90@users.noreply.github.com>
2022-05-26 14:49:17 +0900

    wwww

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_lecture/webview.dart';
+import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -9,43 +11,46 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
-  late TabController _tabController;
+  late List data;
 
   @override
   void initState() {
-    _tabController = TabController(length: 3, vsync: this);
     super.initState();
+    data = [];
+    _getJSONData();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('WebView - Tabbar'),
+        title: const Text('HI'),
       ),
-      body: TabBarView(controller: _tabController, children: const [
-        WebViewPage(initUrl: 'www.google.com'),
-        WebViewPage(initUrl: 'www.naver.com'),
-        WebViewPage(initUrl: 'www.daum.net'),
-      ]),
-      bottomNavigationBar: Container(
-        color: Colors.black,
-        height: 90,
-        child: TabBar(controller: _tabController, tabs: const [
-          Tab(
-            icon: Icon(Icons.face),
-            text: '구글',
-          ),
-          Tab(
-            icon: Icon(Icons.face),
-            text: '네이버',
-          ),
-          Tab(
-            icon: Icon(Icons.scuba_diving_outlined),
-            text: '다음',
-          ),
-        ]),
+      body: Center(
+        child: data.isEmpty
+            ? const CircularProgressIndicator()
+            : ListView.builder(
+                itemCount: data.length,
+                itemBuilder: (context, idx) {
+                  return Card(
+                    child: Text(data[idx]['code']),
+                  );
+                }),
       ),
     );
+  }
+
+  //function
+  Future<bool> _getJSONData() async {
+    var url = Uri.parse('https://zeushahn.github.io/Test/student2.json');
+    var response = await http.get(url);
+
+    var dataConvertedJSON = jsonDecode(utf8.decode(response.bodyBytes));
+    List result = dataConvertedJSON['results'];
+
+    setState(() {
+      data = result;
+    });
+    return true;
   }
 }
