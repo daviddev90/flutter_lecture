4ad0e501655380023cec8c24b83aa51254986134
seungmin9836 <71756020+daviddev90@users.noreply.github.com>
2022-05-27 12:08:22 +0900

    hello

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,5 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:mysql1/mysql1.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -10,71 +8,151 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+class _HomeState extends State<Home> {
+  var settings = ConnectionSettings(
+      host: 'localhost',
+      port: 3306,
+      user: 'root',
+      password: 'qwer1234',
+      db: 'education');
+
+  //Property
+  late TextEditingController codeController;
+  late TextEditingController nameController;
+  late TextEditingController deptController;
+  late TextEditingController phoneController;
+
+  var textfields = [];
   late List data;
 
   @override
   void initState() {
-    super.initState();
+    codeController = TextEditingController();
+    nameController = TextEditingController();
+    deptController = TextEditingController();
+    phoneController = TextEditingController();
+
     data = [];
-    _getJSONData();
+
+    textfields = [
+      {"ct": codeController, "name": "학번"},
+      {"ct": nameController, "name": "이름"},
+      {"ct": deptController, "name": "학과"},
+      {"ct": phoneController, "name": "번호"},
+    ];
+
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HI'),
+        title: const Text('Insert & return for CRUD'),
       ),
       body: Center(
-        child: data.isEmpty
-            ? const CircularProgressIndicator()
-            : ListView.builder(
-                itemCount: data.length,
-                itemBuilder: (context, idx) {
-                  return Padding(
-                    padding: const EdgeInsets.fromLTRB(4, 0, 0, 4),
-                    child: Card(
-                      child: Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: Column(
-                            children: ['Code', 'Name', 'Dept', 'Phone']
-                                .map(
-                                  (e) => Row(
-                                    children: [
-                                      Text(
-                                        e,
-                                        style: const TextStyle(
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                      const SizedBox(
-                                        width: 12,
-                                      ),
-                                      Text(data[idx][e.toLowerCase()]),
-                                    ],
-                                  ),
-                                )
-                                .toList()),
-                      ),
-                    ),
-                  );
-                }),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              ...textfields
+                  .map((e) => TextField(
+                        controller: e["ct"],
+                        decoration:
+                            InputDecoration(labelText: '${e["name"]}을 입력하세요'),
+                        keyboardType: TextInputType.text,
+                      ))
+                  .toList(),
+              const SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    insertAction();
+                  },
+                  child: const Text('입력')),
+              const SizedBox(
+                height: 12,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    listAction();
+                  },
+                  child: const Text('출력')),
+              SizedBox(
+                height: 300,
+                child: ListView.builder(
+                  itemCount: data.length,
+                  itemBuilder: (context, index) {
+                    return Text(data[index]['scode']);
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
 
-  //function
-  Future<bool> _getJSONData() async {
-    var url =
-        Uri.parse('http://localhost:8080/Flutter/studeny_query_flutter.jsp');
-    var response = await http.get(url);
+  _showDialog(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('입력 결과'),
+            content: const Text('입력이 완료되었습니다.'),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK'))
+            ],
+          );
+        });
+  }
 
-    var dataConvertedJSON = jsonDecode(utf8.decode(response.bodyBytes));
-    List result = dataConvertedJSON['results'];
+  errorSnackBar(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+      content: Text('사용자 정보 입력에 문제가 발생 하였습니다.'),
+      duration: Duration(seconds: 1),
+      backgroundColor: Colors.red,
+    ));
+  }
 
+  listAction() async {
+    var conn = await MySqlConnection.connect(settings);
+    var results =
+        await conn.query("select scode, sname, sdept, sphone from student");
     setState(() {
-      data.addAll(result);
+      data = results.toList();
     });
-    return true;
+    conn.close();
+  }
+
+  //functions
+  insertAction() async {
+    var conn = await MySqlConnection.connect(settings);
+
+    var result = await conn.query(
+        'insert into student (scode, sname, sdept, sphone) values (?, ?, ?, ?)',
+        [
+          codeController.text,
+          nameController.text,
+          deptController.text,
+          phoneController.text
+        ]);
+    conn.close();
+  }
+
+  hello() async {
+    // var result = await conn.query(
+    //     'insert into student (scode, sname, sdept, sphone) values (?, ?, ?, ?)',
+    //     ['c123', 'bob', '사학과', 25]);
+
+    // await conn.query(
+    // 'update users set age=? where name=?',
+    // [26, 'Bob']);
   }
 }
